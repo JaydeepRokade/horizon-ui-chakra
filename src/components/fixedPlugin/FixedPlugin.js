0d7c13d78568dada28b131829e2366485330f490
Jaydeep Rokade <jaydeeprokade53@gmail.com>
2025-02-23 12:04:03 +0530

    Optimized FixedPlugin.js and other components. for better readability and performance

--- src/components/fixedPlugin/FixedPlugin.js
+++ src/components/fixedPlugin/FixedPlugin.js
@@ -5,34 +5,35 @@ import { IoMdMoon, IoMdSunny } from "react-icons/io";
 import React from "react";
 
 export default function FixedPlugin(props) {
-  const { ...rest } = props;
   const { colorMode, toggleColorMode } = useColorMode();
-  let bgButton = "linear-gradient(135deg, #868CFF 0%, #4318FF 100%)";
+  const bgButton = "linear-gradient(135deg, #868CFF 0%, #4318FF 100%)";
+  const isRTL = document.documentElement.dir === "rtl";
 
   return (
     <Button
-      {...rest}
-      h='60px'
-      w='60px'
-      zIndex='99'
+      {...props}
+      h="60px"
+      w="60px"
+      zIndex="99"
       bg={bgButton}
-      position='fixed'
-      variant='no-effects'
-      left={document.documentElement.dir === "rtl" ? "35px" : ""}
-      right={document.documentElement.dir === "rtl" ? "" : "35px"}
-      bottom='30px'
-      border='1px solid'
-      borderColor='#6A53FF'
-      borderRadius='50px'
+      position="fixed"
+      variant="no-effects"
+      left={isRTL ? "35px" : "auto"}
+      right={isRTL ? "auto" : "35px"}
+      bottom="30px"
+      border="1px solid"
+      borderColor="#6A53FF"
+      borderRadius="50px"
       onClick={toggleColorMode}
-      display='flex'
-      p='0px'
-      align='center'
-      justify='center'>
+      display="flex"
+      p="0px"
+      alignItems="center"
+      justifyContent="center"
+    >
       <Icon
-        h='24px'
-        w='24px'
-        color='white'
+        h="24px"
+        w="24px"
+        color="white"
         as={colorMode === "light" ? IoMdMoon : IoMdSunny}
       />
     </Button>
